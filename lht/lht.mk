bc1a24992d3b8d89a3962acc48a80f28053eba31
Frank James <frank.a.james@gmail.com>
2019-08-16 09:11:26 +0100

    fix makefile

--- lht/lht.mk
+++ lht/lht.mk
@@ -2,7 +2,7 @@
 lht: ${BINDIR}/lht ${LIBDIR}/liblht.a 
 
 ${LIBDIR}/liblht.a: lht/lht.c include/fju/lht.h 
-	${CC} -c $> ${CFLAGS} 
+	${CC} -c lht/lht.c ${CFLAGS} 
 	${AR} rcs $@ lht.o
 
 ${BINDIR}/lht: lht/lht-main.c ${LIBFJU}

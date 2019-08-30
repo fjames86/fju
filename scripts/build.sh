1ab1ddf0a286f2fac827af75ca22cfdef74d13b3
Frank James <frank.a.james@gmail.com>
2019-08-30 08:43:16 +0100

    script changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
+remoteip=$1
+
 ## stop services and rebuild all 
 sh scripts/rpcd.sh stop 
 make clean all install
 
 ## stop remote service
-scp scripts/rpcd.sh root@169.254.54.129:~
-ssh root@169.254.54.129 sh ~/rpcd.sh stop
+scp scripts/rpcd.sh root@${remoteip}:~
+ssh root@${remoteip} sh ~/rpcd.sh stop
 
 ## install remote
-ssh root@169.254.54.129 mkdir -p /opt/fju
-ssh root@169.254.54.129 mkdir -p /usr/local/bin
-ssh root@169.254.54.129 mkdir -p /usr/local/lib
-scp bin/* root@169.254.54.129:/usr/local/bin
-scp lib/libfju.so root@169.254.54.129:/usr/local/lib
-scp lib/librex.so root@169.254.54.129:~
+ssh root@${remoteip} mkdir -p /opt/fju
+ssh root@${remoteip} mkdir -p /usr/local/bin
+ssh root@${remoteip} mkdir -p /usr/local/lib
+scp bin/* root@${remoteip}:/usr/local/bin
+scp lib/libfju.so root@${remoteip}:/usr/local/lib
+scp lib/librex.so root@${remoteip}:~
 
 ## restart remote services
 sh scripts/rpcd.sh start
-ssh root@169.254.54.129 sh ~/rpcd.sh start
+ssh root@${remoteip} sh ~/rpcd.sh start
 
 
 

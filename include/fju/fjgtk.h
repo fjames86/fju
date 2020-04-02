60f5bb99e38d8d852076740a4bf48b4edb469622
Frank James <frank.a.james@gmail.com>
2020-04-02 04:59:37 +0100

    update

---
+++ include/fju/fjgtk.h
@@ -0,0 +1,14 @@
+
+#ifndef FJGTK_H
+#define FJGTK_H
+
+#include <gtk/gtk.h>
+
+void fjgtk_register( GtkWidget *hwnd, char *name, int *id );
+GtkWidget *fjgtk_get( char *name );
+GtkWidget *fjgtk_get_by_id( int id );
+void fjgtk_add_menu_item( GtkWidget *hmenu, char *name, void (*cb)(), void *data );
+GtkWidget *fjgtk_add_submenu( GtkWidget *hmenu, char *name );
+
+#endif
+

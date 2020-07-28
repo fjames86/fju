f3abf40a6831caa644028b46147d22c6859db37d
Frank James <frank.a.james@gmail.com>
2020-07-28 19:45:49 +0100

    makefile

--- fvm/fvm.mk
+++ fvm/fvm.mk
@@ -1,6 +1,17 @@
 
 
-fvm: ${BINDIR}/fvmc ${LIBDIR}/libfvm.a ${BINDIR}/fvm fvm/programs/test-rpc.fvm fvm/test/test1.fvm fvm/test/test2.fvm fvm/test/test3.fvm fvm/test/test6.fvm
+fvm_programs+=fvm/programs/test-rpc.fvm
+fvm_programs+=fvm/programs/test-service.fvm
+
+fvm_stdlib+=fvm/stdlib/native.asm
+fvm_stdlib+=fvm/stdlib/xdr.asm
+
+fvm_test+=fvm/test/test1.fvm
+fvm_test+=fvm/test/test2.fvm
+fvm_test+=fvm/test/test3.fvm
+fvm_test+=fvm/test/test6.fvm
+
+fvm: ${BINDIR}/fvmc ${LIBDIR}/libfvm.a ${BINDIR}/fvm ${fvm_programs} ${fvm_stdlib} ${fvm_test}
 
 libfvm_source+=fvm/fvm-module.c
 libfvm_source+=fvm/fvm-state.c
@@ -13,7 +24,6 @@ ${LIBDIR}/libfvm.a: ${libfvm_source} include/fju/fvm.h fvm/fvm-private.h
 	${CC} -c ${libfvm_source} ${CFLAGS} 
 	${AR} rcs $@ fvm-module.o fvm-state.o fvm-opcodes.o fvm-native.o fvm-rpc.o fvm-audit.o
 
-fvmc_deps+=
 ${BINDIR}/fvmc: fvm/fvmc.c include/fju/sec.h fvm/fvmc-pascal.c 
 	${CC} -o $@ fvm/fvmc.c fvm/fvmc-pascal.c ${CFLAGS} ${LFLAGS} 
 
@@ -21,12 +31,15 @@ fvm_deps+=${LIBDIR}/libfvm.a
 ${BINDIR}/fvm: fvm/fvm-main.c ${LIBDIR}/libfvm.a 
 	${CC} -o $@ fvm/fvm-main.c ${CFLAGS} ${LFLAGS} 
 
-fvm/programs/test-rpc.fvm: ${BINDIR}/fvmc fvm/programs/test-rpc.pas fvm/programs/test-service.pas
-	${BINDIR}/fvmc -o fvm/programs/test-rpc.fvm fvm/stdlib/native.pas fvm/programs/test-rpc.pas
-	${BINDIR}/fvmc -o fvm/programs/test-service.fvm fvm/stdlib/native.pas fvm/programs/test-service.pas
-
-fvm/stdlib/native.asm: fvm/stdlib/native.pas ${BINDIR}/fvmc
+fvm/stdlib/native.asm: ${BINDIR}/fvmc fvm/stdlib/native.pas
 	${BINDIR}/fvmc fvm/stdlib/native.pas
+fvm/stdlib/xdr.asm: ${BINDIR}/fvmc fvm/stdlib/xdr.pas
+	${BINDIR}/fvmc fvm/stdlib/xdr.pas
+
+fvm/programs/test-rpc.fvm: ${BINDIR}/fvmc fvm/programs/test-rpc.pas 
+	${BINDIR}/fvmc -o fvm/programs/test-rpc.fvm fvm/stdlib/native.pas fvm/programs/test-rpc.pas
+fvm/programs/test-service.fvm: ${BINDIR}/fvmc fvm/programs/test-service.pas
+	${BINDIR}/fvmc -o fvm/programs/test-service.fvm fvm/stdlib/native.pas fvm/programs/test-service.pas
 
 fvm/test/test1.fvm: ${BINDIR}/fvmc ${BINDIR}/fvm fvm/test/test1.asm fvm/stdlib/native.asm
 	${BINDIR}/fvmc -o fvm/test/test1.fvm -I fvm/stdlib/ fvm/test/test1.asm
@@ -38,7 +51,6 @@ fvm/test/test3.fvm: ${BINDIR}/fvmc ${BINDIR}/fvm fvm/test/test3.asm fvm/stdlib/n
 fvm/test/test6.fvm: ${BINDIR}/fvmc fvm/test/test6.pas
 	${BINDIR}/fvmc -o fvm/test/test6.fvm fvm/stdlib/native.pas fvm/test/test6.pas
 
-PROGRAMS+=fvmc
 LIBRARIES+=fvm
 PROGRAMS+=fvm
-
+PROGRAMS+=fvmc

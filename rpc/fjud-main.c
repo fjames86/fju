e43de2004a747635981336d9af8cb84fc5f43fe1
Frank James <frank.a.james@gmail.com>
2020-09-17 11:07:59 +0100

    add cht rsync to daemon

--- rpc/fjud-main.c
+++ rpc/fjud-main.c
@@ -38,6 +38,7 @@
 #include <fju/nls.h>
 #include <fju/freg.h>
 #include <fju/fvm.h>
+#include <fju/cht-rsync.h>
 
 #include "rpc-private.h"
 
@@ -58,6 +59,7 @@ static void init_cb( void ) {
   nls_register();
   freg_register();
   fvm_rpc_register();
+  cht_rsync_initialize();
 }
 
 /* -------- Setup logging ---------------- */

33b48d207b9c26e7191bfa5866c5cdba822135a6
Frank James <frank.a.james@gmail.com>
2020-09-28 08:28:04 +0100

    rename files

--- ef/ef.mk
+++ ef/ef.mk
@@ -1,9 +1,9 @@
 
 ef: ${BINDIR}/ef
 
-${BINDIR}/ef: ef/ef.h ef/ef_shared.c ef/ef.c ef/fastpbkdf2.c
+${BINDIR}/ef: ef/ef.h ef/ef-shared.c ef/ef.c ef/fastpbkdf2.c
 	cc -c -I/usr/include/sys -std=c99 ef/fastpbkdf2.c
-	cc -o ${BINDIR}/ef ef/ef.c ef/ef_shared.c fastpbkdf2.o -lcrypto
+	cc -o ${BINDIR}/ef ef/ef.c ef/ef-shared.c fastpbkdf2.o -lcrypto
 
 PROGRAMS+=ef
 

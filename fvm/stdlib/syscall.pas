b0b0681d28fb49543794de9a9ae5e8f78cec71fe
Frank James <frank.a.james@gmail.com>
2020-12-09 11:52:34 +0000

    wip syscalls

--- fvm/stdlib/syscall.pas
+++ fvm/stdlib/syscall.pas
@@ -1,3 +1,13 @@
 
+{ -*- text -*- }
+
+Const LogLvlTrace = 0x00;
+Const LogLvlDebug = 0x01;
+Const LogLvlInfo = 0x02;
+Const LogLvlWarn = 0x03;
+Const LogLvlError = 0x04;
+Const LogLvlFatal = 0x05;
 Const LogBinary = 0x10;
 Declare Syscall LogWrite(flags : u32, len : u32, buf : opaque) : 1;
+
+Declare Syscall LogReadNexteidHigh : u32, idLow : u32, var len : u32, var buf : opaque, var eidHigh : u32, var eIdLow : u32) : 2;

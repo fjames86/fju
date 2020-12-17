780371f94ee18502dc5cd7b10c24be29c8d6c865
Frank James <frank.a.james@gmail.com>
2020-12-17 15:23:47 +0000

    work on win32 biuld

--- scripts/win32-build.bat
+++ scripts/win32-build.bat
@@ -1,6 +1,16 @@
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" "win32\win32.sln" -Build Debug -Out out.txt
-cat out.txt
-rm out.txt
+REM "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" "win32\win32.sln" -Build "Debug|x64" -Out out.txt
+REM type out.txt
+REM del out.txt 
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" "win32\win32.sln" -Build "Release|x64" -Out out.txt
+type out.txt 
+del out.txt
+mkdir bin\
+copy win32\x64\Release\*.exe bin\
+copy win32\x64\Release\libfju.dll bin\
 
+bin\fvmc.exe -I fvm\stdlib fvm\test\test.pas 
+bin\fvmc.exe -o bin\ -I fvm\stdlib nls\nls.pas
+bin\fvmc.exe -o bin\ -I fvm\stdlib fvm\modules\log.pas
+bin\fvmc.exe -o bin\ -I fvm\stdlib fvm\modules\cht.pas
 

c057548d63a15f0c90399170952ee8ed85cc97f4
Frank James <frank.a.james@gmail.com>
2021-01-04 08:20:50 +0000

    add binary support to shamir

--- sec/shamir.c
+++ sec/shamir.c
@@ -30,7 +30,8 @@ static void usage( char *fmt, ... ) {
 int shamir_main( int argc, char **argv ) {
   int argi;
   int n = 3, k = 2;
-
+  int binarymode = 0;
+  
   argi = 1;
   while( argi < argc ) {
     if( strcmp( argv[argi], "-n" ) == 0 ) {
@@ -41,6 +42,8 @@ int shamir_main( int argc, char **argv ) {
       argi++;
       if( argi >= argc ) usage( NULL );
       k = strtoul( argv[argi], NULL, 10 );
+    } else if( strcmp( argv[argi], "-b" ) == 0 ) {
+      binarymode = 1;
     } else break;
     argi++;
   }
@@ -49,32 +52,42 @@ int shamir_main( int argc, char **argv ) {
   if( argi >= argc ) usage( NULL );
 
   if( strcmp( argv[argi], "split") == 0 ) {
-    int secretlen, i, row;
+    int secretlen, i, row, buflen, sl;
     char *buf;
     struct sec_shamir_share *shares;
     char *hex;
     
     argi++;
     if( argi >= argc ) usage( NULL );
-    buf = argv[argi];
+
+    if( binarymode ) {
+      sl = (3*strlen( argv[argi] )) + 5;
+      buf = malloc( sl );
+      buflen = base64_decode( buf, sl, argv[argi] );
+      if( buflen < 0 ) usage( "Failed to decode input buffer" );
+    } else {
+      buf = argv[argi];
+      buflen = strlen( argv[argi] ) + 1;
+    }
     
-    secretlen = strlen( buf );
+    secretlen = buflen;
     shares = malloc( sizeof(*shares) * n );
     for( i = 0; i < n; i++ ) {
       shares[i].flags = 0;
       shares[i].xval = 0;
-      shares[i].sharebuf = malloc( secretlen + 1 );
+      shares[i].sharebuf = malloc( secretlen );
     }
     sec_shamir_split( (uint8_t *)buf, secretlen, shares, n, k );
 
     hex = malloc( (4*secretlen) / 3 + 5 );
     for( row = 0; row < n; row++ ) {
-      base64_encode( (char *)shares[row].sharebuf, secretlen + 1, hex );
+      base64_encode( (char *)shares[row].sharebuf, secretlen, hex );
       printf( "%s\n", hex );
       free( shares[row].sharebuf );
     }
     free( shares );
     free( hex );
+    if( binarymode ) free( buf );
   } else if( strcmp( argv[argi], "join" ) == 0 ) {
     int k, secretlen, i, sl;
     struct sec_shamir_share *shares;
@@ -102,7 +115,14 @@ int shamir_main( int argc, char **argv ) {
     secret = malloc( secretlen );
     sec_shamir_join( secret, secretlen, shares, k );
 
-    printf( "%s\n", secret );
+    if( binarymode ) {
+      char *sstr = malloc( (4*secretlen) / 3 + 5 );
+      base64_encode( (char *)secret, secretlen, sstr );
+      printf( "%s\n", sstr );
+      free( sstr );
+    } else {
+      printf( "%s\n", secret );
+    }
   } else usage( NULL );
 
   return 0;

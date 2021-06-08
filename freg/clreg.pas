01d696704ec53ceb1dcd9702d8a1628483a9c966
Frank James <frank.a.james@gmail.com>
2021-06-08 08:49:07 +0100

    add snapsotting to clreg

--- freg/clreg.pas
+++ freg/clreg.pas
@@ -38,53 +38,71 @@ Begin
    var regclidH, regclidL : int;
    
    { procedures }
+   Procedure DecodeCommand(buf : opaque, var offset : int, var cmd : int, path : string, var flags : int, var lenp : int, var bufp : opaque)
+   Begin
+	Call XdrDecodeU32(buf,offset,cmd);
+	Call XdrDecodeString(buf,offset,path);
+	If cmd = CmdPut Then
+	Begin
+		Call XdrDecodeU32(buf,offset,flags);
+		Call XdrdecodeU32(buf,offset,lenp);
+		bufp = buf + offset;
+		offset = offset + lenp;
+		If lenp % 4 Then offset = offset + 4 - (lenp % 4);
+	End;
+		
+   End;
+
+   Procedure ApplyCommand(cmd : int, path : string, flags : int, lenp : int, bufp : opaque)
+   Begin
+	var result : int;
+        var entry : FregEntry;
+	
+	If cmd = CmdPut Then
+        Begin	
+	   Call LogWritef(LogLvlTrace,"ClReg Command Put %s Flags=%x len=%d", path,flags,lenp,0);
+	   Syscall FregPut(regh,0,0,path,flags,lenp,bufp,0,0);
+        End
+        Else If cmd = CmdSubkey Then
+        Begin
+   	   Call LogWritef(LogLvlTrace,"ClReg Command Subkey %s", path,0,0,0);	
+	   Syscall FregSubkey(regh,0,0,path,0,0);
+        End
+        Else If cmd = CmdRem Then
+        Begin
+	   Call LogWritef(LogLvlTrace,"ClReg Command Rem %s", path,0,0,0);
+	   Syscall FregEntryByName(regh,0,0,path,entry,result);
+	   
+	   If result Then Begin
+	      Syscall FregRem(regh,entry.idH,entry.idL,result);
+	   End Else Begin
+	      Call LogWritef(LogLvlError,"ClReg Command Rem %s failed",path,0,0,0);
+	   End;
+        End
+        Else Begin
+     	    Call LogWritef(LogLvlError,"ClReg Command unknown command %d path %s", cmd,path,0,0);
+        End;   
+   End;
+   
    Procedure Command(clidH : int,clidL : int,seqH : int,seqL : int,len : int,buf : opaque)
    Begin
-     var cmd : int;
-     var offset : int;	
+     var offset, cmd, flags, lenp, result : int;
+     var bufp : opaque;
      var path : string[1024];
-     var entry : FregEntry;
-     var lenp, flags, result : int;
+
 
      Call LogWritef(LogLvlTrace,"ClReg Command len=%d",len,0,0,0);
      
      offset = 0;
-     Call XdrDecodeU32(buf,offset,cmd);
-     Call XdrDecodeString(buf,offset,path);
-     If cmd = CmdPut Then
-     Begin	
-	Call XdrDecodeU32(buf,offset,flags);
-	Call XdrDecodeU32(buf,offset,lenp);
-
-	Call LogWritef(LogLvlTrace,"ClReg Command Put %s Flags=%x len=%d", path,flags,lenp,0);
-	If (offset + lenp) <= len Then Begin
-	   Syscall FregPut(regh,0,0,path,flags,lenp,buf + offset,0,0);
-	End Else Begin
-	    Call LogWritef(LogLvlError,"ClReg Command Put short buffer",0,0,0,0);
-	End;
-	
-     End
-     Else If cmd = CmdSubkey Then
-     Begin
-	Call LogWritef(LogLvlTrace,"ClReg Command Subkey %s", path,0,0,0);
-	
-	Syscall FregSubkey(regh,0,0,path,0,0);
-     End
-     Else If cmd = CmdRem Then
-     Begin
-	Call LogWritef(LogLvlTrace,"ClReg Command Rem %s", path,0,0,0);
-	
-	Syscall FregEntryByName(regh,0,0,path,entry,result);
-	If result Then Begin
-	   Syscall FregRem(regh,entry.idH,entry.idL,result);
-	End Else Begin
-	   Call LogWritef(LogLvlError,"ClReg Command Rem %s failed",path,0,0,0);
-	End;
-     End
-     Else Begin
-     	  Call LogWritef(LogLvlError,"ClReg Command unknown command %d path %s", cmd,path,0,0);
-     End;
+     Call DecodeCommand(buf,offset,cmd,path,flags,lenp,bufp);
      
+     If cmd = CmdPut && offset > lenp Then
+     Begin
+	Call LogWritef(LogLvlError,"Clreg Command short buffer",0,0,0,0);
+	Return;
+     End;
+
+     Call ApplyCommand(cmd,path,flags,lenp,bufp);
    End;
 
    Procedure SnapsaveValue(snapInfo : int, entryp : int)
@@ -143,7 +161,7 @@ Begin
 
 	Syscall RaftSnapshotSave(clidH,clidL,termH,termL,seqH,seqL,0,0,0);
 
-	{ TODO: walk the tree and append a command for each new entry }
+	{ walk the tree and append a command for each new entry }
 	snapinfo.clidH = clidH;
 	snapinfo.clidL = clidL;
 	snapinfo.termH = termH;
@@ -160,7 +178,9 @@ Begin
 
    Procedure Snapload(clidH : int, clidL : int, len : int, buf : opaque)
    Begin
-	var offset : int;
+	var offset, cmd, flags, lenp : int;
+	var bufp : opaque;
+	var path : string[1024];
 
 	{ the snapshot consists of a list of entries, each of which is a standard command buffer }
 	Call LogWritef(LogLvlTrace,"ClReg Snapload",0,0,0,0);
@@ -168,7 +188,8 @@ Begin
 	offset = 0;
 	While offset < len Do
 	Begin
-	{ todo }
+		Call DecodeCommand(buf,offset,cmd,path,flags,lenp,bufp);
+		Call ApplyCommand(cmd,path,flags,lenp,bufp);
 	End;
 
    End;
@@ -191,29 +212,28 @@ Begin
 
    Procedure Put(path : string, flags : int, len : int, buf : opaque)
    Begin
-	var offset, off : int;
-	var u32 : int;
+	var offset, off, u : int;
 	var u64h, u64l : int;
 	var bufp : opaque[4096];
 
 	offset = 0;
 	off = 0;
-	Call XdrEncodeU32(bufp,offset,CmdPut);
-	Call XdrEncodeString(bufp,offset,path);
-	Call XdrEncodeU32(bufp,offset,flags);
+	Call XdrEncodeU32(bufp, offset, CmdPut);
+	Call XdrEncodeString(bufp, offset, path);
+	Call XdrEncodeU32(bufp, offset, flags);
 	If (flags & FregTypeMask) = FregTypeU32 Then Begin
-	   Call XdrDecodeU32(buf,off,u32);
-	   Call XdrEncodeOpaque(bufp,offset,4,&u32);
+	   Call XdrDecodeU32(buf, off, u);
+	   Call XdrEncodeOpaque(bufp, offset, 4, &u);
 	End Else If (flags & FregTypeMask) = FregTypeU64 Then Begin
-	    Call XdrDecodeU64(buf,off,u64h,u64l);
-	    Call XdrEncodeOpaque(bufp,offset,8,&u64h);
+	    Call XdrDecodeU64(buf, off, u64h, u64l);
+	    Call XdrEncodeOpaque(bufp, offset, 8, &u64h);
 	End Else If (flags & FregTypeMask) = FregTypeString Then Begin
-	    Call XdrDecodeU32(buf,off,u32);
-	    Call XdrEncodeOpaque(bufp,offset,u32,buf + 4);
+	    Call XdrDecodeU32(buf, off, u);
+	    Call XdrEncodeOpaque(bufp, offset, u, buf + 4);
 	End Else 
-	    Call XdrEncodeOpaque(bufp,offset,len,buf);
+	    Call XdrEncodeOpaque(bufp, offset, len, buf);
 	
-	Syscall RaftCommand(regclidH,regclidL,offset,bufp);
+	Syscall RaftCommand(regclidH, regclidL, offset, bufp);
    End;
 
    Procedure Rem(path : string)

d9e7e99bd414a66605e9af8b08e0f2a18c7e1e3e
Frank James <frank.a.james@gmail.com>
2021-07-12 11:57:44 +0100

    wip go

--- go/gfjud/gfjud.go
+++ go/gfjud/gfjud.go
@@ -6,9 +6,10 @@
 package main
 
 import (
-	"fmt"
 	"fju/rpc"
 	"net/http"
+	"sync"
+	"fju/log"
 )
 
 func main() {
@@ -16,25 +17,25 @@ func main() {
 	go fjudHttpServer()
 
 	// kick off rpcd service 
-	fmt.Printf("gfjud: go implementation of fjud starting...\n")
+	log.Info("gfjud: go implementation of fjud starting...\n")
 	rpc.RpcdMain(evtcb, nil)
-	fmt.Printf("gfjud stopped\n")
+	log.Info("gfjud stopped\n")
 }
 
 func evtcb(evt int, cxt interface{}) {
 	switch evt {
 	case rpc.EventInit:
-		fmt.Printf("gfjud started\n")
+		log.Info("gfjud started\n")
 
 		// kick off any services, register rpc programs etc 
 		fjuIterId = rpc.RegisterIterator(goChanIter, 1000)
 
 		rpc.RegisterIterator(func () {
-			fmt.Printf("DUmmy iterator\n")
+			log.Info("DUmmy iterator\n")
 		}, 1000)
 		
 	case rpc.EventClose:
-		fmt.Printf("gfjud stopping\n")
+		log.Info("gfjud stopping\n")
 	default:
 	}
 }
@@ -46,36 +47,75 @@ func fjudHttpServer() {
 
 var fjuIterId int
 
-type fjuMsg interface{}
+var fjuMsgLock sync.Mutex 
+type fjuMsg struct {
+	cb func(interface{})
+	arg interface{}
+}
+
 var fjuMsgQueue = make([]fjuMsg, 0)
 
 func fjuMsgPost(msg fjuMsg) {
+	fjuMsgLock.Lock()
 	fjuMsgQueue = append(fjuMsgQueue, msg)
+	fjuMsgLock.Unlock()
 }
-func fjuMsgPop() fjuMsg {
-	if len(fjuMsgQueue) == 0 {
-		return nil
-	}
+func fjuMsgPop() (fjuMsg,bool) {
+	var msg fjuMsg
+	found := false
 	
-	msg := fjuMsgQueue[0]
-	fjuMsgQueue = fjuMsgQueue[1:]
-	return msg
+	fjuMsgLock.Lock()
+	if len(fjuMsgQueue) > 0 {
+		msg = fjuMsgQueue[0]
+		fjuMsgQueue = fjuMsgQueue[1:]
+		found = true
+	}
+	fjuMsgLock.Unlock()
+
+	return msg, found
 }
 
 func fjuHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("received request to /fju\n")
-	FjuChannelPost("my channel message")
+	log.Info("received request to /fju\n")
+	FjuExecute(func(arg interface{}) {
+		s := arg.(string)
+		log.Info("%s\n", s)
+	}, "my channel message")
 }
 
-func FjuChannelPost(val interface{}) {
-	fjuMsgPost((fjuMsg)(val))
+// FjuExecuteAsync allows running a function in the context of fjud main thread
+// It simply schedules the function to be run and returns immediately.
+func FjuExecuteAsync(cb func(interface{}), arg interface{}) {
+	msg := fjuMsg{ cb: cb, arg: arg }
+	fjuMsgPost(msg)
 	rpc.ScheduleIterator(fjuIterId)
 }
 
+// FjuExecute allows running a function in the context of fjud main thread
+// and waiting for it to be completed. This function blocks until the function has returned.
+func FjuExecute(cb func(interface{}), arg interface{}) {
+
+	l := sync.Mutex{}
+	cv := sync.NewCond(&l)
+	
+	FjuExecuteAsync(func (arg interface{}) {
+		// execute the cb 
+		cb(arg)
+
+		// wake the calling thread 
+		cv.Signal()
+	}, arg)
+
+	cv.L.Lock()
+	cv.Wait()
+	cv.L.Unlock()
+}
+
+// executes in context of rpcd thread 
 func goChanIter() {
-	fmt.Printf("goChanIter\n")
-	value := fjuMsgPop()
-	if value != nil {
-		fmt.Printf("goChanIter got a value: %v", value)
+	log.Trace("goChanIter\n")
+	msg, found := fjuMsgPop()
+	if found && msg.cb != nil{
+		msg.cb(msg.arg)
 	}
 }
